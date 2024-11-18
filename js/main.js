c805f395a63afa52bd03050c85dfd03ddc88c139
IsabelXue15 <isabelxue@college.harvard.edu>
2024-11-17 23:34:10 -0500

    pushing everything

--- js/main.js
+++ js/main.js
@@ -4,3 +4,7 @@ document.addEventListener("DOMContentLoaded", function () {
         dataPath: "data/revenue_data.csv", // Path to the CSV file
     });
 });
+
+brandGrowthVis = new BrandGrowth({
+    parentElement: "#VisContainer1",
+    data: "data/revenue_data.csv"});
